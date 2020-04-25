eb2b2e4d1831b133455ace24358d2f971e722c4d
hyun chae <daniel.chae.a@gmail.com>
2020-04-25 18:41:54 +0700

    uibuild

---
+++ build/precache-manifest.c356b7432ec719b24fb7f8eebb180cba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c78a3ce10aa32417a5d5b2fadc1269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a846d9e1e209c6cdf73d",
+    "url": "/static/css/main.8dfe1e5a.chunk.css"
+  },
+  {
+    "revision": "50b2e459d25667e2841c",
+    "url": "/static/js/2.37c0a557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37c0a557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a846d9e1e209c6cdf73d",
+    "url": "/static/js/main.656df244.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
